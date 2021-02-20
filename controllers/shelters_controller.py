b282b77a5d66f96a227cadfe8ea9665c3435d1f4
jairoAussie <jairoaussie@gmail.com>
2021-02-20 14:21:40 +1000

    create and delete shelter from the browser

--- controllers/shelters_controller.py
+++ controllers/shelters_controller.py
@@ -4,9 +4,10 @@ from models.User import User
 from main import db
 from schemas.ShelterSchema import shelter_schema, shelters_schema
 from schemas.AnimalSchema import animalSchema, animalsSchema
-from flask import Blueprint, request, jsonify, render_template, abort
+from flask import Blueprint, request, jsonify, render_template, abort, redirect, url_for
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from services.auth_service import verify_user
+from flask_login import login_required, current_user
 
 shelters = Blueprint('shelters',__name__, url_prefix="/shelters")
 
@@ -19,30 +20,63 @@ def shelter_index():
     #return jsonify(shelters_schema.dump(rs))
 
 @shelters.route("/", methods=["POST"])
-@jwt_required
-@verify_user
-def shelter_create(user=None):
+@login_required
+def shelter_create():
 
-    shelter = Shelter.query.filter_by(user_id=user.id).first()
+    name = request.form.get("name")
+    address = request.form.get("address")
+    city = request.form.get("city")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+
+    #make sure that the logged in user doesn't have a shelter
+    shelter = Shelter.query.filter_by(user_id=current_user.id).first()
     #print (shelter)
     if shelter:
         return abort(400, description= "Not authorised to create more than one shelter")
+
     #print(request.json)
-    shelter_fields = shelter_schema.load(request.json)
+    #shelter_fields = shelter_schema.load(request.json)
     #create a new Shelter object, with the data received in the request
     new_shelter = Shelter()
-    new_shelter.name = shelter_fields["name"]
-    new_shelter.email = shelter_fields["email"]
-    new_shelter.phone = shelter_fields["phone"]
-    new_shelter.address = shelter_fields["address"]
-    new_shelter.city = shelter_fields["city"]
-    new_shelter.user_id = user.id
+    new_shelter.name = name
+    new_shelter.email = email
+    new_shelter.phone = phone
+    new_shelter.address = address
+    new_shelter.city = city
+    new_shelter.user_id = current_user.id
 
     #add a new shelter to the db
     db.session.add(new_shelter)
     db.session.commit()
 
-    return jsonify(shelter_schema.dump(new_shelter))
+    #return jsonify(shelter_schema.dump(new_shelter))
+    return redirect(url_for('shelters.shelter_index'))
+# @shelters.route("/", methods=["POST"])
+# @jwt_required
+# @verify_user
+# def shelter_create(user=None):
+
+#     shelter = Shelter.query.filter_by(user_id=user.id).first()
+#     #print (shelter)
+#     if shelter:
+#         return abort(400, description= "Not authorised to create more than one shelter")
+#     #print(request.json)
+#     shelter_fields = shelter_schema.load(request.json)
+#     #create a new Shelter object, with the data received in the request
+#     new_shelter = Shelter()
+#     new_shelter.name = shelter_fields["name"]
+#     new_shelter.email = shelter_fields["email"]
+#     new_shelter.phone = shelter_fields["phone"]
+#     new_shelter.address = shelter_fields["address"]
+#     new_shelter.city = shelter_fields["city"]
+#     new_shelter.user_id = user.id
+
+#     #add a new shelter to the db
+#     db.session.add(new_shelter)
+#     db.session.commit()
+
+#     return jsonify(shelter_schema.dump(new_shelter))
 
 @shelters.route("/<int:id>", methods=["GET"])
 def shelter_show(id):
@@ -58,10 +92,10 @@ def shelter_animals_show(id):
     return render_template("animals_index.html", anims = animals)
     #return jsonify(animalsSchema.dump(animals))
 
-@shelters.route("/<int:id>", methods=["DELETE"])
-@jwt_required
-@verify_user
-def shelter_delete(id, user=None):
+#@shelters.route("/<int:id>", methods=["DELETE"])
+@shelters.route("/delete/<int:id>", methods=["GET"])
+@login_required
+def shelter_delete(id):
     # user_id = get_jwt_identity()
     # user = User.query.get(user_id)
 
@@ -70,13 +104,14 @@ def shelter_delete(id, user=None):
 
 
     #shelter = Shelter.query.get(id)
-    shelter = Shelter.query.filter_by(id=id, user_id=user.id).first()
+    shelter = Shelter.query.filter_by(id=id, user_id=current_user.id).first()
     if not shelter:
         return abort(400, description="Not authorized to delete other people's shelters")
 
     db.session.delete(shelter)
     db.session.commit()
-    return jsonify(shelter_schema.dump(shelter))
+    #return jsonify(shelter_schema.dump(shelter))
+    return redirect(url_for('shelters.shelter_index'))
 
 @shelters.route("/<int:id>", methods=["PUT","PATCH"])
 @jwt_required
@@ -88,3 +123,8 @@ def shelter_update(id, user=None):
     shelters.update(shelter_fields)
     db.session.commit()
     return jsonify(shelter_schema.dump(shelters[0]))
+
+
+@shelters.route("/new", methods=["GET"])
+def new_shelter():
+    return render_template("new_shelter.html")
